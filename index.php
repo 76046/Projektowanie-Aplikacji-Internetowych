c1471d6a29d0b55b6382d954088cae493e76d5c1
Marek K <marek.korzeniewski1996@gmail.com>
2020-10-21 21:24:06 +0200

    Menu0.005
    Menu

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>KANT-MEN</title>
+        <link href="CSS/styleGlowne.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav class="nawigacja-menu">
+                <div class="czesc-gorna">
+                    <div class="logo">
+                        Logo
+                    </div>
+                    <div class="logowanie-rejestracja">
+                        Logowanie/Rejestracja
+                    </div>
+                </div>
+                <div class="czesc-dolna">
+                    <div class="strona-glowna">Strona główna</div>
+                    <div class="kurs-walut">Kursy Walut</div>
+                    <div class="kurs-kruszcow">Kursy Kruszców</div>
+                    <div class="forum">Forum</div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>

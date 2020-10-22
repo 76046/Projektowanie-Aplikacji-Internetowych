e005b5f6551936e48ab99a413931de6c16a18ea1
adkow <a.kowalewski.student@gmail.com>
2020-10-22 11:43:12 +0200

    Adding a calculator

--- index.php
+++ index.php
@@ -8,6 +8,25 @@
         <meta charset="utf-8">
         <title>KANT-MEN</title>
         <link href="CSS/styleGlowne.css" rel="stylesheet" type="text/css">
+        <script>
+            function convertCurrency() {
+                var from = Document.getElementById("from").value;
+                var to = Document.getElementById("to").value;
+                var xmlhttp = new XMLHttpRequest();
+                var url = "http://api.fixer.io/latest?symbols=" + from + "," + to;
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+                xmlhttp.onreadystatechange = function() {
+                    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        var result = xmlhttp.responseText;
+                        var jsResult = JSON.parse(result);
+                        var oneUnit = jsResult.rates[to]/jsResult.rates[from];
+                        var amt = document.getElementById("fromAmount").value;
+                        document.getElementById("toAmount").value = (oneUnit*amt).toFixed(2);
+                    }
+                }
+            }
+        </script>
     </head>
     <body>
         <header>
@@ -30,6 +49,35 @@
         </header>
 
         <main>
+            <div id="box">
+                <h2>Currency converter</h2>
+                <table>
+                    <tr>
+                        <td><input id="fromAmount" type="text" value="100" onkeyup="convertCurrency();"></td>
+                        <td>
+                            <select id="from" onchange="convertCurrency();">
+                                <option value="PLN" selected>Polski złoty (PLN)</option>
+                                <option value="EUR">Euro (EUR)</option>
+                                <option value="USD">Dolar amerykański (USD)</option>
+                                <option value="GBP">Funt brytyjski (GBP)</option>
+                                <option value="CHF">Frank szwajcarski (CHF)</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><input id="toAmount" type="text" disabled></td>
+                        <td>
+                            <select id="to" onchange="convertCurrency();">
+                                <option value="PLN">Polski złoty (PLN)</option>
+                                <option value="EUR">Euro (EUR)</option>
+                                <option value="USD" selected>Dolar amerykański (USD)</option>
+                                <option value="GBP">Funt brytyjski (GBP)</option>
+                                <option value="CHF">Frank szwajcarski (CHF)</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </div>
             <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt distinctio ipsam magnam molestias
                 odit optio quae? Debitis fugiat incidunt ipsam ipsum magnam officiis omnis pariatur perspiciatis
                 praesentium repudiandae! Inventore, obcaecati.

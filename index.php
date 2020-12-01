86d4d8338bd0903c1d4d2c5378ba52b7335b1186
adkow <a.kowalewski.student@gmail.com>
2020-12-01 14:04:52 +0100

    Index and Stopka add structure.

--- index.php
+++ index.php
@@ -32,6 +32,146 @@
                     </select>
                 </div>
             </div>
+
+            <div class="">
+                <div>
+                    <div>
+                        <div>
+                            <h2>nagłówek tekstu</h2>
+                        </div>
+                        <div>
+                            <p>treść tekstu</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="zdjęcie">
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <img src="" alt="ikonka1">
+                        </div>
+                        <div>
+                            <h3>nagłówek1</h3>
+                        </div>
+                        <div>
+                            <p>treść tekstu1</p>
+                        </div>
+                    </div> 
+                    <div>
+                        <div>
+                            <img src="" alt="ikonka2">
+                        </div>
+                        <div>
+                            <h3>nagłówek2</h3>
+                        </div>
+                        <div>
+                            <p>treść tekstu2</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="ikonka3">
+                        </div>
+                        <div>
+                            <h3>nagłówek3</h3>
+                        </div>
+                        <div>
+                            <p>treść tekstu3</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="slider">
+                <div class="slides">
+
+                    <input type="radio" name="radio-btn" id="radio1">
+                    <input type="radio" name="radio-btn" id="radio2">
+                    <input type="radio" name="radio-btn" id="radio3">
+                    <input type="radio" name="radio-btn" id="radio4">
+
+                    <div class="slide first">
+                        <img src="" alt="zdjęcie1">
+                        <div>
+                            <h2>Nagłówek artykułu 1</h2>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="" alt="zdjęcie2">
+                        <div>
+                            <h2>Nagłówek artykułu 2</h2>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="" alt="zdjęcie3">
+                        <div>
+                            <h2>Nagłówek artykułu 3</h2>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="" alt="zdjęcie4">
+                        <div>
+                            <h2>Nagłówek artykułu 4</h2>
+                        </div>
+                    </div>
+
+                    <div class="navigation-auto">
+                        <div class="auto-btn1"></div>
+                        <div class="auto-btn2"></div>
+                        <div class="auto-btn3"></div>
+                        <div class="auto-btn4"></div>
+                    </div>
+
+                </div>
+
+                <div class="navigation-manual">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                    <label for="radio4" class="manual-btn"></label>
+                </div>
+
+            </div>
+
+            <div>
+                <div>
+                    <h2>Nagrody i wyróżnienia</h2>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <img src="" alt="obrazek1">
+                            </div>
+                        </div>
+                        <div>
+                            <p>info o nagrodzie1</p> 
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <img src="" alt="obrazek2">
+                            </div>
+                        </div>
+                        <div>
+                            <p>info o nagrodzie2</p> 
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <img src="" alt="obrazek3">
+                            </div>
+                        </div>
+                        <div>
+                            <p>info o nagrodzie3</p> 
+                        </div>
+                    </div> 
+                </div>
+            </div>
         </main>
     <?php
     include_once('rysowanieStopki.php');
--- rysowanieStopki.php
+++ rysowanieStopki.php
@@ -3,7 +3,9 @@ function rysowanieStopki()
 {
     ?>
     <footer>
-
+        <div>
+            <p>&copy; 2020 - 2020 Kant-men.pl</p>
+        </div>
     </footer>
     <?php
 }

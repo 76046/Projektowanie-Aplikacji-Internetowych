70e50eb9ec3c2ae8a6d05b1a15fc4274e3bcaa55
adkow <a.kowalewski.student@gmail.com>
2020-12-03 10:25:08 +0100

    index and css

--- index.php
+++ index.php
@@ -33,51 +33,51 @@
                 </div>
             </div>
 
-            <div class="">
-                <div>
-                    <div>
-                        <div>
+            <div class="informacje">
+                <div class="transakcja">
+                    <div class="transakcja-lewa">
+                        <div class="transakcja-lewa-naglowek">
                             <h2>nagłówek tekstu</h2>
                         </div>
-                        <div>
+                        <div class="transakcja-lewa-tresc">
                             <p>treść tekstu</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="transakcja-prawa">
                         <img src="" alt="zdjęcie">
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <div>
+                <div class="benefity">
+                    <div class="benefit1">
+                        <div class="benefit1-img">
                             <img src="" alt="ikonka1">
                         </div>
-                        <div>
+                        <div class="benefit1-naglowek">
                             <h3>nagłówek1</h3>
                         </div>
-                        <div>
+                        <div class="benefit1-tresc">
                             <p>treść tekstu1</p>
                         </div>
-                    </div> 
+                    </div class="benefit2"> 
                     <div>
-                        <div>
+                        <div class="benefit2-img">
                             <img src="" alt="ikonka2">
                         </div>
-                        <div>
+                        <div class="benefit2-naglowek">
                             <h3>nagłówek2</h3>
                         </div>
-                        <div>
+                        <div class="benefit2-tresc">
                             <p>treść tekstu2</p>
                         </div>
                     </div>
-                    <div>
-                        <div>
+                    <div class="benefit3">
+                        <div class="benefit3-img">
                             <img src="" alt="ikonka3">
                         </div>
-                        <div>
+                        <div class="benefit3-naglowek">
                             <h3>nagłówek3</h3>
                         </div>
-                        <div>
+                        <div class="benefit3-tresc">
                             <p>treść tekstu3</p>
                         </div>
                     </div>
@@ -135,38 +135,32 @@
 
             </div>
 
-            <div>
-                <div>
+            <div class="nagrody">
+                <div class="nagrody-tytul">
                     <h2>Nagrody i wyróżnienia</h2>
                 </div>
-                <div>
-                    <div>
-                        <div>
-                            <div>
-                                <img src="" alt="obrazek1">
-                            </div>
+                <div class="nagrody-dolne">
+                    <div class="nagroda1">
+                        <div class="nagroda1-img">
+                            <img src="" alt="obrazek1">
                         </div>
-                        <div>
+                        <div class="nagroda1-info">
                             <p>info o nagrodzie1</p> 
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            <div>
-                                <img src="" alt="obrazek2">
-                            </div>
+                    <div class="nagroda2">
+                        <div class="nagroda2-img">
+                            <img src="" alt="obrazek2">
                         </div>
-                        <div>
+                        <div class="nagroda2-info">
                             <p>info o nagrodzie2</p> 
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            <div>
-                                <img src="" alt="obrazek3">
-                            </div>
+                    <div class="nagroda3">
+                        <div class="nagroda3-img">
+                            <img src="" alt="obrazek3">
                         </div>
-                        <div>
+                        <div class="nagroda3-info">
                             <p>info o nagrodzie3</p> 
                         </div>
                     </div> 

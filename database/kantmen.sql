cdacd264a9435ff3b1abbc892c6cf1dce133545b
adkow <a.kowalewski.student@gmail.com>
2020-12-08 16:34:58 +0100

    new commit

--- database/kantmen.sql
+++ database/kantmen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 07 Gru 2020, 19:10
+-- Czas generowania: 08 Gru 2020, 16:31
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.11
 
@@ -61,8 +61,24 @@ CREATE TABLE `komentarz` (
 CREATE TABLE `kruszec` (
   `ID_KRUSZEC` int(10) NOT NULL,
   `NAZWA` varchar(50) COLLATE utf8_polish_ci NOT NULL,
-  `CENA` double NOT NULL
+  `CENA` double NOT NULL,
+  `JEDNOSTKA` varchar(30) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `kruszec`
+--
+
+INSERT INTO `kruszec` (`ID_KRUSZEC`, `NAZWA`, `CENA`, `JEDNOSTKA`) VALUES
+(1, 'Pallad', 8613.82, 'PLN/uncja'),
+(2, 'Pallad', 8613.82, 'PLN/uncja'),
+(3, 'Złoto', 6885.89, 'PLN/uncja'),
+(4, 'Cynk', 10301.53, 'PLN/tona'),
+(5, 'Aluminium', 7393.68, 'PLN/tona'),
+(6, 'Ołów', 7666.66, 'PLN/tona'),
+(7, 'Srebro', 90.93, 'PLN/uncja'),
+(8, 'Miedź', 28240.22, 'PLN/tona'),
+(9, 'Platyna', 3812.58, 'PLN/uncja');
 
 -- --------------------------------------------------------
 
@@ -102,6 +118,32 @@ CREATE TABLE `waluta` (
   `KUPNO` double NOT NULL,
   `SPRZEDAZ` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `waluta`
+--
+
+INSERT INTO `waluta` (`ID_WALUTA`, `KOD_WALUTA`, `NAZWA`, `KUPNO`, `SPRZEDAZ`) VALUES
+(1, 'EUR', 'Euro', 4.4597, 4.4897),
+(2, 'USD', 'Dolar amerykański', 3.6775, 3.7075),
+(3, 'CHF', 'Frank szwajcarski', 4.1411, 4.1811),
+(4, 'GBP', 'Funt brytyjski', 4.9047, 4.9444),
+(5, 'AUD', 'Dolar australijski', 2.7233, 2.7533),
+(6, 'BGN', 'Lew bułgarski', 2.2758, 2.3008),
+(7, 'CAD', 'Dolar kanadyjski', 2.8705, 2.9005),
+(8, 'CZK', 'Korona czeska', 0.1683, 0.1703),
+(9, 'DKK', 'Korona duńska', 0.5981, 0.6041),
+(10, 'HKD', 'Dolar hongkoński', 0.4742, 0.4784),
+(11, 'HRK', 'Kuna chorwacka', 0.5903, 0.5963),
+(12, 'MXN', 'Peso meksykańskie', 0.1844, 0.187),
+(13, 'NOK', 'Korona norweska', 0.4185, 0.4245),
+(14, 'NZD', 'Dolar nowozelandzki', 2.5815, 2.6115),
+(15, 'RON', 'Lej rumuński', 0.9124, 0.9244),
+(16, 'RUB', 'Rubel rosyjski', 0.0494, 0.0514),
+(17, 'SEK', 'Korona szwedzka', 0.4354, 0.4404),
+(18, 'SGD', 'Dolar singapurski', 2.7473, 2.7743),
+(19, 'TRY', 'Lira turecka', 0.464, 0.48),
+(20, 'ZAR', 'Rand południowoafrykański', 0.2431, 0.2465);
 
 -- --------------------------------------------------------
 
@@ -185,7 +227,7 @@ ALTER TABLE `komentarz`
 -- AUTO_INCREMENT dla tabeli `kruszec`
 --
 ALTER TABLE `kruszec`
-  MODIFY `ID_KRUSZEC` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_KRUSZEC` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT dla tabeli `user`
@@ -197,7 +239,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT dla tabeli `waluta`
 --
 ALTER TABLE `waluta`
-  MODIFY `ID_WALUTA` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_WALUTA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT dla tabeli `watek`
--- rysowanieMenu.php
+++ rysowanieMenu.php
@@ -6,14 +6,16 @@ function rysowanieGlownegoMenu()
     <header>
         <nav class="nawigacja-menu">
             <div class="nawigacja-czesc-gorna">
-                <div class="logo">Logo</div>
+                <div class="logo"><a href="index.php"><img src="img/logo-duze.png" alt="logo"></a></div>
                 <div class="logowanie-rejestracja"><a href="logowanie.php">Logowanie</a>/ <a href="rejestracja.php">Rejestracja</a></div>
+                <div style="clear: both;"></div>
             </div>
             <div class="nawigacja-czesc-dolna">
                 <div class="strona-glowna"> <a href="index.php"  >Strona główna</a></div>
                 <div class="kurs-walut"> <a href="waluty.php">Kursy Walut</a></div>
                 <div class="kurs-kruszcow"> <a href="kruszce.php">Kursy Kruszców</a></div>
                 <div class="forum"> <a href="forum.php">Forum</a></div>
+                <div style="clear: both;"></div>
             </div>
         </nav>
     </header>

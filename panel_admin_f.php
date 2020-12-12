c48037af1736cc3c15240229c00fb61c37196699
Marek K <marek.korzeniewski1996@gmail.com>
2020-12-12 16:50:00 +0100

    Pai

---
+++ panel_admin_f.php
@@ -0,0 +1,149 @@
+<?php
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>KANT-MEN</title>
+    <link href="CSS/styleGlowne.css" rel="stylesheet" type="text/css">
+    <link href="CSS/stylePanelAdmina.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<?php
+include_once('rysowanieMenu.php');
+rysowanieGlownegoMenu();
+?>
+
+<main>
+    <article class="artykol">
+
+        <div class="tytul_artykulu">
+            Panel Administracyjny
+        </div>
+
+
+        <section class="sekcja">
+
+            <table>
+                <tr>
+                    <th class="panel_artykuly">
+                        <a href="panel_admina_a.php">Artykuly</a>
+                    </th>
+                    <th class="panel_uzytkownicy">
+                        <a href="panel_admina_u.php">Użytkownicy</a>
+                    </th>
+                    <th class="panel_forum">
+                        <a href="panel_admina_f.php">Forum</a>
+                    </th>
+                    <th class="panel_zgloszenia">
+                        <a href="panel_admina_z.php">Zgłoszenia</a>
+                    </th>
+                </tr>
+            </table>
+            <!--            --><?php
+            //
+            //            require_once "polaczeniezMySQL.php";
+            //
+            //            $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+            //
+            //            if($polaczenie->connect_errno!=0)
+            //            {
+            //                echo "Error: ".$polaczenie->connect_errno;//." Opis ".$polaczenie->connect_error;
+            //            }
+            //            else
+            //            {
+            //            $rezultat = $polaczenie->query("SELECT * FROM artykul");
+            //
+            //            if(!$rezultat)
+            //            {
+            //                throw new Exception($polaczenie->error);
+            //            }
+            //            else
+            //            {
+            //            $iterator = 0;
+            //            ?>
+            <div class="tabliczki">
+                <table>
+                    <tr>
+                        <th class="ID">ID Artykulu</th>
+                        <th class="uzytkownik_wst">Użytkownik wstawiajacy</th>
+                        <th class="miejsce_mema">Czas wstawienia</th>
+                        <th class="Lkom">Liczba odwiedzin</th>
+                        <th class="Padmin">Panel Administratora</th>
+                    </tr>
+                    <!--                    --><?php
+                    //                    while($row = mysqli_fetch_array($rezultat))
+                    //                    {
+                    //
+                    //                        $rezultat_loginu = mysqli_fetch_array($polaczenie->query();
+                    //                        //var_dump($rezultat_loginu);
+                    //
+                    //                        if(($iterator%2)==0)
+                    //                        {
+                    //                            ?>
+                    <tr>
+                        <td class="ID"></td>
+                        <td class="uzytkownik_wst"></td>
+                        <td class="miejsce_mema"></td>
+                        <td class="Lkom"></td>
+                        <td class="Padmin">
+
+                            <a><button type="button" class="mute_glowna" name="butt_glowna" method="post">Glow</button></a>
+
+                            <a><button type="button" class="mute_glowna" name="butt_glowna" method="post">Pocz</button></a>
+
+                            <a><button type="button" class="ban_button" name="butt_ban" method="post">UiB</button></a>
+
+                            <a ><button type="button" class="del_button" name="butt_del" method="post">Usu</button></a>
+                        </td>
+
+                    </tr>
+                    <tr class="codrugi">
+                        <td class="ID"></td>
+                        <td class="uzytkownik_wst"></td>
+                        <td class="miejsce_mema"></td>
+                        <td class="Lkom"></td>
+                        <td class="Padmin">
+
+                            <a><button type="button" class="mute_glowna" name="butt_glowna" method="post">Glow</button></a>
+
+                            <a><button type="button" class="mute_glowna" name="butt_glowna" method="post">Pocz</button></a>
+
+
+                            <a><button type="button" class="ban_button" name="butt_ban" method="post">UiB</button></a>
+
+                            <a ><button type="button" class="del_button" name="butt_del" method="post">Usu</button></a>
+
+                        </td>
+                    </tr>
+                    <!--                            --><?php
+                    //                        }
+                    //                        $iterator++;
+                    //                    }
+                    //                    }
+                    //                    $rezultat->close();
+                    //                    }
+                    //                    $polaczenie->close();
+                    //
+                    //                    ?>
+
+
+
+                </table>
+            </div>
+
+
+
+        </section>
+
+
+
+    </article>
+
+
+</main>
+</body>
+</html>
--- waluty.php
+++ waluty.php
@@ -23,28 +23,7 @@ rysowanieGlownegoMenu();
         <div class="pas-tytulowy">
                 Kursy Walut
         </div>
-        <?php
-                    require_once "polaczeniezMySQL.php";
 
-                    $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
-                    
-                    if($polaczenie->connect_errno!=0)
-                    {
-                        echo "Error: ".$polaczenie->connect_errno;
-                    }
-                    else
-                    {
-                        $rezultat = $polaczenie->query("SELECT * FROM waluta");
-
-                        if(!$rezultat)
-                        {
-                        throw new Exception($polaczenie->error);
-                        }
-                        else
-                        {
-                        while($row = mysqli_fetch_assoc($rezultat))
-                        {
-        ?>
         <section class="tablica-walut">
             <div class="tabliczki">
                 <table>
@@ -54,6 +33,28 @@ rysowanieGlownegoMenu();
                         <th class="sprzedaz-waluty">Sprzedaż</th>
                         <th class="operacje-waluty">Operacje</th>
                     </tr>
+                    <?php
+                    require_once "polaczeniezMySQL.php";
+
+                    $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+
+                    if($polaczenie->connect_errno!=0)
+                    {
+                        echo "Error: ".$polaczenie->connect_errno;
+                    }
+                    else
+                    {
+                    $rezultat = $polaczenie->query("SELECT * FROM waluta");
+
+                    if(!$rezultat)
+                    {
+                        throw new Exception($polaczenie->error);
+                    }
+                    else
+                    {
+                    while($row = mysqli_fetch_assoc($rezultat))
+                    {
+                    ?>
                     <tr class="co-drugi">
                         <td class="nazwa-waluty">
                             <div class="flaga"></div>
@@ -110,16 +111,16 @@ rysowanieGlownegoMenu();
 
                         </td>
                     </tr> -->
-
-                </table>
-        </section>
-        <?php
+                        <?php
                     }
                     }
-                    $rezultat->close();
+                        $rezultat->close();
                     }
                     $polaczenie->close();
-                ?>
+                    ?>
+                </table>
+        </section>
+
     </article>
 
     <?php

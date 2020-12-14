9a4aceded769d53dd48183cfeadfb6e4d228715d
Marek K <marek.korzeniewski1996@gmail.com>
2020-12-14 13:26:25 +0100

    Pai

---
+++ dodaj_artykuł.php
@@ -0,0 +1,47 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>KANT-MEN</title>
+    <link href="CSS/styleGlowne.css" rel="stylesheet" type="text/css">
+    <link href="CSS/styleDodaj_artykul.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<?php
+include_once('rysowanieMenu.php');
+rysowanieGlownegoMenu();
+?>
+
+
+<main>
+    <section>
+        <div class="pas-tytulowy">
+            Dodanie nowego wątku
+        </div>
+        <br class="pas-tytulowy">
+        <p>Temat :</p>
+        <center><input type="text"></center>
+        </br>
+        <hr>
+        </br>
+        <p>Treść :</p>
+        <center><textarea></textarea></center>
+        </div>
+        </br>
+        <hr>
+        </br>
+        <div class="pas-tytulowy">
+            Dodaj
+        </div>
+    </section>
+</main>
+
+<?php
+include_once('rysowanieStopki.php');
+rysowanieStopki();
+?>
+</body>
+</html>

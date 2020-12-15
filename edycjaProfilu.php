989641ff15330ad9203706fe21f90e5b289a19a1
adkow <a.kowalewski.student@gmail.com>
2020-12-15 23:53:04 +0100

    Pai

--- edycjaProfilu.php
+++ edycjaProfilu.php
@@ -43,6 +43,7 @@ rysowanieGlownegoMenu();
 <main>
     <article class="artykol">
 
+    <form method="post" enctype="multipart/form-data">
         <div class="lewybaner">
             <div class="pro_avatar">
 
@@ -86,36 +87,36 @@ rysowanieGlownegoMenu();
                 </div>
                 <div class="staty" id="nazwisko">
                     <?php $nazwisko = $row['NAZWISKO'];
-                    echo '<input type="text" name="imie" value="'.$nazwisko.'">' ?>
+                    echo '<input type="text" name="nazwisko" value="'.$nazwisko.'">' ?>
                 </div>
                 <div class="staty" id="email">
                     <?php $email = $row['EMAIL'];
-                    echo '<input type="text" name="imie" value="'.$email.'">' ?>
+                    echo '<input type="text" name="email" value="'.$email.'">' ?>
                 </div>
                 <div class="staty" id="miasto">
                     <?php $miasto = $row['MIASTO'];
-                    echo '<input type="text" name="imie" value="'.$miasto.'">' ?>
+                    echo '<input type="text" name="miasto" value="'.$miasto.'">' ?>
                 </div>
                 <div class="staty" id="wiek">
                     <?php $wiek = $row['WIEK'];
-                    echo '<input type="text" name="imie" value="'.$wiek.'">' ?>
+                    echo '<input type="text" name="wiek" value="'.$wiek.'">' ?>
                 </div>
                 <div class="staty" id="kraj">
                     <?php $kraj = $row['KRAJ'];
-                    echo '<input type="text" name="imie" value="'.$kraj.'">' ?>
+                    echo '<input type="text" name="kraj" value="'.$kraj.'">' ?>
                 </div>
                 </div>
                 <div class="o_sobie">Coś o sobie:</div>
                 <div class="staty-opis" id="opis">
                     <?php $opisProfilu = $row['OPIS_PROFILU'];
-                    echo '<input type="textarea" name="imie" value="'.$opisProfilu.'">' ?>
+                    echo '<input type="textarea" name="opis" value="'.$opisProfilu.'">' ?>
                 </div>
             </div>
             <div class="edycjaProfilu"><?php echo '<a href="profilowe.php?user=' . $row['ID_USER'] . '">'?><input type="submit" value="Edytuj profil" name="edytuj_profil"></a></div>
 
         </div>
         <div class="koniec"></div>
-
+        </form>
     </article>
     <?php
     }
@@ -131,7 +132,25 @@ include_once('rysowanieStopki.php');
 rysowanieStopki();
 ?>
 
-
-
 </body>
 </html>
+<?php
+      if(isset($_POST['edytuj_profil'])){
+        $zdjecie = $_POST['zdjecie'];
+        $imie = $_POST['imie'];
+        $nazwisko = $_POST['nazwisko'];
+        $email = $_POST['email'];
+        $miasto = $_POST['miasto'];
+        $wiek = $_POST['wiek'];
+        $kraj = $_POST['kraj'];
+        $opis = $_POST['opis'];
+        //$query = "UPDATE user SET IMIE='$imie',NAZWISKO='$nazwisko', EMAIL='$email', MIASTO='$miasto', WIEK=$wiek, KRAJ='$kraj', OPIS='$opis' WHERE USER_ID=".$_GET['user'];
+        // $result = mysqli_query($db, $query) or die(mysqli_error($db));
+        if($polaczenie->query("UPDATE user SET IMIE='$imie',NAZWISKO='$nazwisko', EMAIL='$email', MIASTO='$miasto', WIEK=$wiek, KRAJ='$kraj', OPIS='$opis' WHERE USER_ID=".$_GET['user'])){
+            echo ' ';
+        }  
+        else{
+            echo $polaczenie->error;
+        }     
+       }              
+?>

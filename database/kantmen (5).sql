41676afde55b2e7fdf648e6bbc8027dbf0cf67bc
Marek K <marek.korzeniewski1996@gmail.com>
2020-12-19 23:47:30 +0100

    pai

--- database/kantmen (5).sql
+++ database/kantmen (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 19 Gru 2020, 21:20
+-- Czas generowania: 19 Gru 2020, 23:46
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.2
 
@@ -171,8 +171,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`ID_USER`, `LOGIN`, `HASLO`, `IMIE`, `NAZWISKO`, `EMAIL`, `WIEK`, `MIASTO`, `KRAJ`, `OPIS_PROFILU`, `ZDJECIE`, `STW_WATKI`, `LICZ_KOMENTARZY`, `UPRAWNIENIA`) VALUES
 (1, 'Adam', '3c6e3550e97b872da67938d34f3740f11c94102a5bfd3ddd4501820e980b5ca778c2fbe88d2695affe9a53bb5064c5a8a717360db3ecb87e2c0c74feea2f37a8', 'Marek', 'Korzeniewski', 'email1@wp.pl', 15, 'Białys', 'Polska', ':O', NULL, 0, 0, 'USER'),
-(4, 'Admin', '3c6e3550e97b872da67938d34f3740f11c94102a5bfd3ddd4501820e980b5ca778c2fbe88d2695affe9a53bb5064c5a8a717360db3ecb87e2c0c74feea2f37a8', 'imie1', 'nazwisko1', 'email@wp.pl', 11, 'Białystok', 'Polska', NULL, NULL, 0, 0, 'USER'),
-(7, 'Marek', '3c6e3550e97b872da67938d34f3740f11c94102a5bfd3ddd4501820e980b5ca778c2fbe88d2695affe9a53bb5064c5a8a717360db3ecb87e2c0c74feea2f37a8', 'M', 'K', 'markor88@wp.pl', 16, 'Kraków', 'Poland', '', NULL, 0, 2, 'ADMIN'),
+(4, 'Admin', '3c6e3550e97b872da67938d34f3740f11c94102a5bfd3ddd4501820e980b5ca778c2fbe88d2695affe9a53bb5064c5a8a717360db3ecb87e2c0c74feea2f37a8', 'imie1', 'nazwisko1', 'email@wp.pl', 11, 'Białystok', 'Polska', NULL, NULL, 0, 0, 'ADMIN'),
+(7, 'Marek', '3c6e3550e97b872da67938d34f3740f11c94102a5bfd3ddd4501820e980b5ca778c2fbe88d2695affe9a53bb5064c5a8a717360db3ecb87e2c0c74feea2f37a8', 'M', 'K', 'markor88@wp.pl', 16, 'Kraków', 'Poland', '', NULL, 0, 2, 'MUTED'),
 (8, 'KONTOZ BANOWANE', 'zaq1@WSX', '', '', '', 0, '', '', NULL, NULL, 0, 0, 'MOD');
 
 -- --------------------------------------------------------
@@ -245,7 +245,7 @@ CREATE TABLE `watek` (
   `LICZBA_KOMENTARZY` int(10) NOT NULL DEFAULT 0,
   `ILOSC_ODWIEDZIN` int(10) NOT NULL DEFAULT 0,
   `OCENA` int(10) NOT NULL DEFAULT 0,
-  `STATUS` varchar(10) COLLATE utf8_polish_ci NOT NULL DEFAULT 'UKRYTE'
+  `STATUS` varchar(20) COLLATE utf8_polish_ci NOT NULL DEFAULT 'UKRYTE'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
@@ -253,9 +253,9 @@ CREATE TABLE `watek` (
 --
 
 INSERT INTO `watek` (`ID_WATEK`, `ID_USER`, `TEMAT`, `DATA`, `TRESC_WATKU`, `LICZBA_KOMENTARZY`, `ILOSC_ODWIEDZIN`, `OCENA`, `STATUS`) VALUES
-(2, 1, 'temattemattemat', '2020-12-11 18:41:17', 'tresctresctresctresctresctresctresctresc', 0, 306, -1, 'UKRYTE'),
-(3, 1, 'temattemattemat', '2020-12-11 18:42:57', 'tresctresctresctresctresctresctresctresc', 0, 6, 0, 'UKRYTE'),
-(4, 8, 'Czy dolar będzie tanieć', '2020-12-19 18:30:15', 'hahahha nie będzie.', 0, 1, 0, 'UKRYTE'),
+(2, 1, 'temattemattemat', '2020-12-11 18:41:17', 'tresctresctresctresctresctresctresctresc', 0, 309, -1, 'UKRYTE'),
+(3, 1, 'temattemattemat', '2020-12-11 18:42:57', 'tresctresctresctresctresctresctresctresc', 0, 11, 0, 'USUNIETE'),
+(4, 8, 'Czy dolar będzie tanieć', '2020-12-19 18:30:15', 'hahahha nie będzie.', 0, 1, 0, 'USUNIETE'),
 (5, 1, 'Euro', '2020-12-19 18:30:15', 'Euro oo', 0, 0, 0, 'UKRYTE');
 
 -- --------------------------------------------------------

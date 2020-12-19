7247f6198be4a7c556c4f8ffd25baa202631dec7
Marek K <marek.korzeniewski1996@gmail.com>
2020-12-19 20:59:54 +0100

    PAI

--- admin.php
+++ admin.php
@@ -18,23 +18,27 @@ if($polaczenie->connect_errno!=0)
 }
 else
 {
-//usuwanie watku
+    if($_GET['z_poziomu']=='usera'){
+
+        //usuwanie komentarza
+        if ($_GET['komentarz']) {
+            $rezultat = $polaczenie->query("SELECT * FROM `ocenakomentarz` WHERE `ID_KOMENTARZA_OCENIONEGO` =" . $_GET['komentarz']);
+            if (!$rezultat) {
+                throw new Exception($polaczenie->error);
+            } else {
+                while ($jednaocena = mysqli_fetch_array($rezultat)) {
+                    $polaczenie->query("DELETE FROM `ocenakomentarz` WHERE `ocenakomentarz`.`ID_KOMENTARZA_OCENIONEGO` =".$jednaocena['ID_KOMENTARZA_OCENIONEGO']);
+                }
+            }
+            $polaczenie->query("DELETE FROM `komentarz` WHERE `ID_KOMENTARZ` =" . $_GET['komentarz']);
+        }
+
+
+    }
+//usuwanie watku poprzez ukrycie go
 //    if ($_GET['watek']) {
 //        $rezultat = $polaczenie->query("SELECT * FROM `watek` WHERE `ID_WATEK` =" . $_GET['watek']);
 //    }
-//usuwanie komentarza
-
-    if ($_GET['komentarz']) {
-        $rezultat = $polaczenie->query("SELECT * FROM `ocenakomentarz` WHERE `ID_KOMENTARZA_OCENIONEGO` =" . $_GET['komentarz']);
-        if (!$rezultat) {
-            throw new Exception($polaczenie->error);
-        } else {
-            while ($jednaocena = mysqli_fetch_array($rezultat)) {
-                $polaczenie->query("DELETE FROM `ocenakomentarz` WHERE `ocenakomentarz`.`ID_KOMENTARZA_OCENIONEGO` =".$jednaocena['ID_KOMENTARZA_OCENIONEGO']);
-            }
-        }
-        $polaczenie->query("DELETE FROM `komentarz` WHERE `ID_KOMENTARZ` =" . $_GET['komentarz']);
-    }
 
 //akceptacja postu admina bądź też nie
 
--- edycjaProfilu.php
+++ edycjaProfilu.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once "PolaczeniezMySQL.php";
+require_once "polaczeniezMySQL.php";
 
 $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
 
@@ -85,40 +85,43 @@ rysowanieGlownegoMenu();
                 echo '<input type="hidden" name="id" value="'.$id.'">' ?>
                 <div class="staty" id="imie">
                     <?php $imie = $row['IMIE'];
-                    echo '<input type="text" name="imie" value="'.$imie.'">' ?>
+                    echo '<input type="text" name="imie" value="'.$imie.'">'; ?>
                 </div>
                 <div class="staty" id="nazwisko">
                     <?php $nazwisko = $row['NAZWISKO'];
-                    echo '<input type="text" name="nazwisko" value="'.$nazwisko.'">' ?>
+                    echo '<input type="text" name="nazwisko" value="'.$nazwisko.'">'; ?>
                 </div>
                 <div class="staty" id="email">
                     <?php $email = $row['EMAIL'];
-                    echo '<input type="text" name="email" value="'.$email.'">' ?>
+                    echo '<input type="text" name="email" value="'.$email.'">'; ?>
                 </div>
                 <div class="staty" id="miasto">
                     <?php $miasto = $row['MIASTO'];
-                    echo '<input type="text" name="miasto" value="'.$miasto.'">' ?>
+                    echo '<input type="text" name="miasto" value="'.$miasto.'">'; ?>
                 </div>
                 <div class="staty" id="wiek">
                     <?php $wiek = $row['WIEK'];
-                    echo '<input type="text" name="wiek" value="'.$wiek.'">' ?>
+                    echo '<input type="text" name="wiek" value="'.$wiek.'">'; ?>
                 </div>
                 <div class="staty" id="kraj">
                     <?php $kraj = $row['KRAJ'];
-                    echo '<input type="text" name="kraj" value="'.$kraj.'">' ?>
+
+                    echo '<input type="text" name="kraj" value="'.$kraj.'">'; ?>
+
                 </div>
                 </div>
                 <div class="o_sobie">Coś o sobie:</div>
                 <div class="staty-opis" id="opis">
                     <?php $opisProfilu = $row['OPIS_PROFILU'];
-                    echo '<input type="textarea" name="opis" value="'.$opisProfilu.'">' ?>
+                    echo '<input type="textarea" name="opis" value="'.$opisProfilu.'">'; ?>
                 </div>
             </div>
-            <div class="edycjaProfilu"><?php echo '<a href="profilowe.php?user=' . $row['ID_USER'] . '">'?><input type="submit" value="Edytuj profil" name="edytuj_profil"></a></div>
+
+            <div class="edycjaProfilu"><input type="submit" value="Zapisz i wróć" name="edytuj_profil"></div>
 
         </div>
         <div class="koniec"></div>
-        </form>
+    </form>
     </article>
     <?php
     }
@@ -150,12 +153,12 @@ rysowanieStopki();
         //$query = "UPDATE user SET IMIE='$imie',NAZWISKO='$nazwisko', EMAIL='$email', MIASTO='$miasto', WIEK=$wiek, KRAJ='$kraj', OPIS='$opis' WHERE USER_ID=".$_GET['user'];
         // $result = mysqli_query($db, $query) or die(mysqli_error($db));
         if($polaczenie->query("UPDATE user SET IMIE='$imie',NAZWISKO='$nazwisko', EMAIL='$email', MIASTO='$miasto', WIEK=$wiek, KRAJ='$kraj', OPIS_PROFILU='$opis' WHERE ID_USER='$id'")){
-            header('refresh : 1');
-        }  
+            echo("<script>document.location.href = 'profilowe.php?user=".$row['ID_USER']."';</script>");
+        }
         else{
             echo $polaczenie->error;
-        }     
-       }
+        }
 
-       $polaczenie->close();              
+       }
+       $polaczenie->close();
 ?>
